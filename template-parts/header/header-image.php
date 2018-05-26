d281d4b3158e68fb60ed266da8f8359cdbd860f3
Eric Rucker <bhtooefr@bhtooefr.org>
2018-05-26 17:18:58 -0400

    Update header-image.php

--- template-parts/header/header-image.php
+++ template-parts/header/header-image.php
@@ -11,10 +11,6 @@
 ?>
 <div class="custom-header">
 
-		<div class="custom-header-media">
-			<?php the_custom_header_markup(); ?>
-		</div>
-
 	<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
 
 </div><!-- .custom-header -->
