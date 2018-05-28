a0e86a37d901cc0b6dd2bf9a42099a5172d52b3b
Eric Rucker <bhtooefr@uncannyvalley.bhtooefr.org>
2018-05-28 07:00:08 -0400

    tweaking search results, updating version to 0.1 as it's now running in production

--- template-parts/post/content-excerpt.php
+++ template-parts/post/content-excerpt.php
@@ -17,6 +17,16 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header class="entry-header">
+		<?php
+		if ( is_front_page() && ! is_home() ) {
+
+			// The excerpt is being displayed within a front page section, so it's a lower hierarchy than h2.
+			the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' );
+		} else {
+			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		}
+		?>
+		
 		<?php if ( 'post' === get_post_type() ) : ?>
 			<div class="entry-meta">
 				<?php
@@ -29,19 +39,10 @@
 				<?php twentyseventeen_edit_link(); ?>
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
-
-		<?php
-		if ( is_front_page() && ! is_home() ) {
-
-			// The excerpt is being displayed within a front page section, so it's a lower hierarchy than h2.
-			the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' );
-		} else {
-			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-		}
-		?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 </article><!-- #post-## -->
+<hr>
